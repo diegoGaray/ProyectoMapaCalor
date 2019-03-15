055cbb634c5154675466f1d08c5a95396dacf86e
d.garayleiva <d.garayleiva@gmail.com>
2019-03-15 08:27:38 -0300

    Second commit

--- app/src/main/java/com/appbusso/mapsapp/MapsActivity.java
+++ app/src/main/java/com/appbusso/mapsapp/MapsActivity.java
@@ -4,25 +4,44 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
+import android.support.constraint.Placeholder;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+
+
+import com.appbusso.mapsapp.models.PlaceInfo;
+import com.arlib.floatingsearchview.FloatingSearchView;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
@@ -40,13 +59,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Scanner;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import cameo.code.placeautocomplete.PlaceAutoCompleteFragment;
+import cameo.code.placeautocomplete.PlaceModel;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, PlaceAutoCompleteFragment.onPlaceSelectedListener{
 
     private GoogleMap mMap;
     private HeatmapTileProvider mProvider;
@@ -62,6 +85,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private LocationManager locationManager;
     private LocationListener myLocationListener;
+    private GoogleApiClient mGoogleApiClient;
+    private FloatingSearchView mSearchView;
+
+    private BottomSheetBehavior mBottomSheetBehavior;
+    private PlaceAutocompleteAdapter placeAutocompleteAdapter;
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(
+            new LatLng(-40, -168), new LatLng(71, 136));
 
     int[] colors = {
             Color.rgb(102, 225, 0), // green
@@ -72,6 +102,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             0.2f, 0.3f
     };
 
+    private PlaceInfo mPlace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +112,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        LinearLayout llMain = (LinearLayout) findViewById(R.id.llbottomsheet);
+        mBottomSheetBehavior = BottomSheetBehavior.from(llMain);
+
+
+        mBottomSheetBehavior.setPeekHeight(300);
+        mBottomSheetBehavior.setHideable(false);
+        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_DRAGGING);
+
+        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    Toast.makeText(getApplicationContext(), "STATE_EXPANDED", Toast.LENGTH_LONG).show();
+                    mMap.setPadding(0, 0, 0, bottomSheet.getHeight());
+//                    googleMap.animateCamera(CameraUpdateFactory.zoomTo(8.0f));
+                 //   clusterMarker();
+                } else if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                    Toast.makeText(getApplicationContext(), "STATE_COLLAPSED", Toast.LENGTH_LONG).show();
+                    mMap.setPadding(0, 0, 0, 310);
+//                    googleMap.animateCamera(CameraUpdateFactory.zoomTo(10.0f));
+                  //  clusterMarker();
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+
         mDatabase = FirebaseDatabase.getInstance().getReference();
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED &&
@@ -103,23 +166,77 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         {
             gpsTracker.showSettingsAlert();
         }
+
     }
 
+    public void getAdapter(){
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .build();
+
+
+        placeAutocompleteAdapter = new PlaceAutocompleteAdapter(this, mGoogleApiClient, LAT_LNG_BOUNDS,null);
+        mSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
+            @Override
+            public void onSearchTextChanged(String oldQuery, final String newQuery) {
+
+                //get suggestions based on newQuery
+
+                //pass them on to the search view
+
+            }
+        });
+
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_in_night));
         checkLocation();
+        // Setting a click event handler for the map
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                // Setting the title for the marker.
+                // This will be displayed on taping the marker
+                //markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+
+                Geocoder geocoder=new Geocoder(getApplicationContext());
+
+                try {
+                    List<Address> addressList=geocoder.getFromLocation(latLng.latitude,latLng.longitude,10);
+
+                    for(Address address:addressList){
+                        Log.d("TAG",address.getAddressLine(0));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // Clears the previously touched position
+                mMap.clear();
+
+                // Animating to the touched position
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                // Placing a marker on the touched position
+                mMap.addMarker(markerOptions);
+
+
+            }
+        });
         // Add a marker in Sydney and move the camera
         //LatLng sydney = new LatLng(-34, 151);
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
@@ -139,8 +256,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng getCurrent = new LatLng(currentLat, currentLng);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(getCurrent));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-    }
+        mMap.setMyLocationEnabled(true);
 
+
+    }
 
 
     @Override
@@ -264,7 +383,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 //.gradient(gradient)
                // .opacity(100)
                 .build();
-        mProvider.setRadius(100);
+        mProvider.setRadius(50);
 
 // Add the tile overlay to the map.
         mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
@@ -332,5 +451,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         return list;
+    }
+
+    @Override
+    public void onPlaceSelected(PlaceModel placeModel) {
+        LatLng selectedPlace = new LatLng(placeModel.getLat(), placeModel.getLng());
+
+        mMap.addMarker(new MarkerOptions().position(selectedPlace).title("Marker in "+placeModel.getMainPlace()));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(selectedPlace));
+    }
+
+    @Override
+    public String getWebApiKey() {
+        return getString(R.string.google_maps_key);
     }
 }
